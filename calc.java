f5240f86a57a05214876458c5c45f571ca649c3b
dibya0007 <46745664+dibya0007@users.noreply.github.com>
2020-10-20 10:56:10 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -2,7 +2,10 @@ class  Calc{
   public static void add(int a,int b){
    System.out.println (a+b);
   }
+   public static void add(int a,int b){
+   System.out.println (a-b);
+  }
   public static void main(String []args){
-    add(10,20);
+    add(80,20);
   }
 }

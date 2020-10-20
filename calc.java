42b666f5491296572d5458777a20327e3607675e
dibya0007 <46745664+dibya0007@users.noreply.github.com>
2020-10-20 10:34:46 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,6 +1,6 @@
 class  Calc{
   public static int add(int a,int b){
-   System.out.println( return (a+b));
+   System.out.println (a+b);
   }
   public static void main(String []args){
     add(10,20);
